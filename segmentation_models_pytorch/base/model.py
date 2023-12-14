d62ece7dbf97fc7a78c85dbfbef13d961af98738
dotrann1412 <dotrann.1412@gmail.com>
2023-12-15 02:22:48 +0700

    detach feature extraction

--- segmentation_models_pytorch/base/model.py
+++ segmentation_models_pytorch/base/model.py
@@ -10,7 +10,6 @@ class SegmentationModel(torch.nn.Module):
             init.initialize_head(self.classification_head)
 
     def check_input_shape(self, x):
-
         h, w = x.shape[-2:]
         output_stride = self.encoder.output_stride
         if h % output_stride != 0 or w % output_stride != 0:
@@ -20,15 +19,16 @@ class SegmentationModel(torch.nn.Module):
                 f"Wrong input shape height={h}, width={w}. Expected image height and width "
                 f"divisible by {output_stride}. Consider pad your images to shape ({new_h}, {new_w})."
             )
+    
+    @torch.jit.export
+    def feature(self, x):
+        """Sequentially pass `x` trough model`s encoder, decoder and heads"""
+        return self.encoder(x)
 
-    def forward(self, x):
+    def forward(self, features):
         """Sequentially pass `x` trough model`s encoder, decoder and heads"""
 
-        self.check_input_shape(x)
-
-        features = self.encoder(x)
         decoder_output = self.decoder(*features)
-
         masks = self.segmentation_head(decoder_output)
 
         if self.classification_head is not None:
@@ -37,6 +37,7 @@ class SegmentationModel(torch.nn.Module):
 
         return masks
 
+    @torch.jit.export
     @torch.no_grad()
     def predict(self, x):
         """Inference method. Switch model to `eval` mode, call `.forward(x)` with `torch.no_grad()`
